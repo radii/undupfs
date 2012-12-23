a4210328814fb1bd0e8a89d78d116125cd85211e
Andy Isaacson <adi@hexapodia.org>
2012-12-23 14:10:57 -0800

    src/test/: add test harness and first functional test

--- src/test/runtest.sh
+++ src/test/runtest.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-set -x
+# set -x
 
+target=$1
 testdir=`dirname $0`
+tmpdir=`mktemp -d`
+
+export target testdir tmpdir
+
+trap "rm -rf $tmpdir" QUIT EXIT INT HUP
 
 . $testdir/funcs
 
-test_numpass=0
+begin
 
-for t in $testdir/t.*; do
-    bash -c ". $testdir/funcs; . $t" && success $t || fail $t
+for s in $testdir/t.*; do
+    t=$(basename $s)
+    r=$tmpdir/results.$$.$t
+    export result=$r
+    out=$tmpdir/out.$$.$t
+    echo "=== $t ==="
+    bash -c ". $testdir/setup; . $s" 2>$out 99>$r && win $t || lose $t $?
+    lose $t $?
 done
+
+finish
