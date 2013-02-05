737698e36c2e17ccd4d0fb7e674bcf3a2a97e26a
Andy Isaacson <adi@hexapodia.org>
2013-02-04 22:35:47 -0800

    bloom.h: add function doc comments

--- src/bloom.h
+++ src/bloom.h
@@ -11,12 +11,35 @@ struct bloom_params {
     int bitperf;  // bits consumed per F_i
 };
 
+/*
+ * Set up a cohort of Bloom filters.  bloom_setup initializes shared state and
+ * validates the settings.
+ *
+ * @sz: size of the Bloom bitarray, in bits.
+ * @nb: number of bits set per key inserted into filter.
+ * @kl: length of input keys, in bytes.
+ */
 struct bloom_params *bloom_setup(int sz, int nb, int kl);
 
+/*
+ * Initialize a single filter given the established settings.
+ *
+ * @b: bitarray to initialize.  Must be of the appropriate size.
+ */
 void bloom_init(struct bloom_params *p, u8 *b);
 
+/* Insert KEY into filter B.
+ *
+ * Returns 1 if the key collided with existing entries (that is, all of the bits
+ * set due to B were already set).  Returns 0 if B caused a bit to be set.
+ */
 int bloom_insert(struct bloom_params *p, u8 *b, u8 *key);
 
+/* Check if KEY is present in filter B.
+ *
+ * Returns 1 if KEY may have been inserted (all of the bits set due to KEY are
+ * set).  Returns 0 if KEY was not inserted.
+ */
 int bloom_present(struct bloom_params *p, u8 *b, u8 *key);
 
 #endif /* BLOOM_H_ */
